cf074987a19a8a1952af74fe95f2063c18d6a363
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-13 16:00:51 -0300

    Update Program.cs

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,2 +1,75 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-Console.WriteLine("teste");
+
+//TestaBuscarPalavra();
+TestaMediana(amostra);
+
+void TestaArrayInt ()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 50;
+    idades[3] = 42;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
+    int acumulador = 0;
+
+    for ( int i = 0; i < idades.Length; i++ )
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Índice [{i}] = {idade}");
+        acumulador += idade;
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Media de idades = {media}");
+}
+
+void TestaBuscarPalavra ()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0;i < arrayDePalavras.Length;i++ )
+    {
+        Console.WriteLine($"Digite {i + 1}° Paralavras:");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.WriteLine("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}.");
+            break;
+        }
+    }
+}
+
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(7, 0);
+amostra.SetValue(8, 1);
+amostra.SetValue(6, 2);
+amostra.SetValue(3, 3);
+amostra.SetValue(10, 4);
+
+void TestaMediana(Array array)
+{
+    if (array == null || array.Length == 0)
+    {
+        Console.WriteLine("Array para cáculo da media está vazio ou nulo");
+    }
+
+    double[] numerosOrdenados = (double [])array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ?
+        numerosOrdenados[meio] : numerosOrdenados[meio] + (numerosOrdenados[meio - 1]) / 2;
+
+    Console.WriteLine($"Com base na amostra a mediana = {mediana}");
+}
